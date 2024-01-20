cae56694838b793333752f02af93f23d8f123826
Supriti Mondal <155663734+Titli990@users.noreply.github.com>
2024-01-20 21:01:46 +0530

    Delete Test1 directory

--- Test1/Multiplication.java
+++
@@ -1,9 +0,0 @@
-package Test2;
-
-public class Multiplication {
-    public static void main(String args[]){
-
-        int i=25,j=5;
-        System.out.println(i+" * "+j+" = "+i*j);
-    }
-}
--- Test1/Print.java
+++
@@ -1,8 +0,0 @@
-package Test2;
-
-public class Print {
-    public static void main(String args[]){
-
-        System.out.print("Hello\nI'm Supriti");
-    }
-}
