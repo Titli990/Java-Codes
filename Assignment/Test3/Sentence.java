2abbd0f95bd3ec1dab82121ce1791fa516d6c348
Supriti Mondal <155663734+Titli990@users.noreply.github.com>
2024-02-01 20:53:35 +0530

    Sentence.java

--- Assignment/Test3/Sentence.java
+++ Assignment/Test3/Sentence.java
@@ -18,11 +18,11 @@ public class Sentence {
         //Number of vowels and consonants.
         int v=0,c=0;
         for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='A'||str.charAt(i)=='E'||str.charAt(i)=='I'||str.charAt(i)=='O'||str.charAt(i)=='U'||
-                    str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+            if((str.charAt(i)=='A')||(str.charAt(i)=='E')||(str.charAt(i)=='I')||(str.charAt(i)=='O')||(str.charAt(i)=='U')||
+                    (str.charAt(i)=='a')||(str.charAt(i)=='e')||(str.charAt(i)=='i')||(str.charAt(i)=='o')||(str.charAt(i)=='u')){
                 v++;
             }
-            else {
+            else if ((str.charAt(i) >= 'A' && str.charAt(i) < 'Z') || (str.charAt(i) > 'a' && str.charAt(i) < 'z')){
                 c++;
             }
         }
